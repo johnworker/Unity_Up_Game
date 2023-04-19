5e51b9d8321c290694a8220339b1d0e193b18a87
hunte <besscomdark@gmail.com>
2023-04-20 07:01:51 +0800

    敵人設置

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace kan
+{
+    public class Enemy : MonoBehaviour
+    {
+        public float speed;
+        public int health;
+    }
+}
