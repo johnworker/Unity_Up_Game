8968a5cbe6f557ee72470082b4005b2b67d6f807
hunte <besscomdark@gmail.com>
2023-04-21 06:54:44 +0800

    敵人腳本製作

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,5 +12,29 @@ namespace kan
         SpriteRenderer spriteRenderer;
         Rigidbody2D rigid;
         public Sprite[] sprites;
+
+        void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            rigid = GetComponent<Rigidbody2D>();
+            rigid.velocity = Vector2.down * speed;
+        }
+
+        void OnHit(int dmg)
+        {
+            health -= dmg;
+            spriteRenderer.sprite = sprites[1];
+            ReturnSpirt();
+
+            if(health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void ReturnSpirt()
+        {
+            spriteRenderer.sprite = sprites[0];
+        }
     }
 }
